24d675d6928b61eff09ad09351815de4e0cf8fa2
Madeleine Gallegos Lyons <107013907+MigLyons@users.noreply.github.com>
2023-10-01 14:49:35 -0600

    partially reframed for django

--- .venv/Scripts/activate.bat
+++ .venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\Miish\desktop\wad\miglyons.github.io\.venv
+set VIRTUAL_ENV=C:\Users\Miish\desktop\portfolio\miglyons.github.io\.venv
 
 if not defined PROMPT set PROMPT=$P$G
 
---
+++ miglyons_portfolio/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class MiglyonsPortfolioConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'miglyons_portfolio'
